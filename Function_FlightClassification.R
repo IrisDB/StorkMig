3a1ee0ace201b4bd921b14f62d66f85252f2f352
Iris <idbontekoe@hotmail.com>
2022-02-11 11:28:35 +0100

    Update Function_FlightClassification.R

--- Function_FlightClassification.R
+++ Function_FlightClassification.R
@@ -1,1 +1,17 @@
 
+#### In translation
+
+#-------------------------------------------------------------#
+#                                                             #
+#--------------- Function FlightClassification ---------------#
+#                                                             #
+#-------------------------------------------------------------#
+
+# Project: 
+# Authors: Iris Bontekoe
+# Date started: 14 May 2020
+# Date last modified: 11 February 2022
+# R version: 
+# Description: This script determines for every data point whether the stork was flying and whether it was climbing or gliding.
+
+
